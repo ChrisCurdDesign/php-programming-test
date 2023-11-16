9949de8f580eb70f49ef583909026cdf824ddb0a
ccdappdev <107686006+ccdappdev@users.noreply.github.com>
2023-11-16 14:53:26 +0000

    Update class.ccd_dependencies.php

--- class.ccd_dependencies.php
+++ class.ccd_dependencies.php
@@ -6,7 +6,7 @@ class dependencies {
 
     public static function get_dependencies_html(array $dependencies) : string {
         // TODO: Implement get_dependencies_html() method.
-        $dependencies_html = '';
+        $dependencies_html = 'whoa!';
         return $dependencies_html;
     }
 
